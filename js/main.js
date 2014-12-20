80d5bbe737fd3bdb80101d86af57e6edfe099030
Patrick Do <patrickdo@gmail.com>
2014-12-20 10:20:18 -0800

    initial

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/*global document, $, console */
+/*jshint globalstrict: true*/
+/* enable strict mode */
+"use strict";
+
+$(document).ready(function() {
+
+	$('button').click(function() {
+		$(this)
+			.css('border-width','5px')
+			.css('border-color', '#f0ad4e')
+			.css('font-weight', 'bold');
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+});
